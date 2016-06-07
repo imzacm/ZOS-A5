9fb2e55b7c0b442f0544d86527c26ce8f65f25d6
Zac McChesney <imzacm@gmail.com>
2016-06-08 00:29:13 +0100

    might have fixed problem with grid system

--- scrn.c
+++ scrn.c
@@ -158,8 +158,9 @@ void settextcolor(unsigned char forecolor, unsigned char backcolor)
 
 int charInArr(char * arr, char ch)
 {
+	int length = sizeof(arr) / sizeof(arr[0]);
 	int ret = 0;
-	for (int i = 0; i < arr.size(); i++)
+	for (int i = 0; i < length; i++)
 	{
 		if (arr[i] == ch)
 		{
@@ -173,10 +174,10 @@ int charInArr(char * arr, char ch)
 	return ret;
 }
 
-char * nums = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-char * lowerCase = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-char * upperCase = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-char * symbols = {'!', '£', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', '@', '~', '#', '<', '>', ',', '.', '?', '/', '|', '\\', '`', '¬'};
+char nums[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+char lowerCase[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+char upperCase[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+char symbols[30] = {'!', '£', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '[', ']', '{', '}', '@', '~', '#', '<', '>', ',', '.', '?', '/', '|', '\\', '`', '¬'};
 char Rows[25][80];
 void writeToScreen(int x, int y, char toPlace)
 {
